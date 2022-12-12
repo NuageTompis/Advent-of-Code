a9eb269859807e3cd162d35969e91ca3d69ffdd3
NuageTompis <88559558+NuageTompis@users.noreply.github.com>
2022-12-12 19:33:50 +0100

    Updated the Useful Tools

--- 2022/modules/UT.js
+++ 2022/modules/UT.js
@@ -14,3 +14,54 @@ export const Desc = (a, b) => {
 export const Asc = (a, b) => {
   return a - b;
 };
+
+// Calculates the highest common divider of a and b through Euclide's theorem
+export const PGCD = (a, b) => {
+  let c;
+  while (b != 0) {
+    c = b;
+    b = a % b;
+    a = c;
+  }
+  return a;
+};
+
+// Calculates the smallest common multiple of a and b
+export const PPCM = (a, b) => {
+  return Math.abs((a * b) / PGCD(a, b));
+};
+
+// Given two arrays, return their term-by-term sum
+export const ArrSum = (arr1, arr2) => {
+  return arr1.map((cur, ind) => cur + arr2[ind]);
+};
+
+// Same thing but for the substracion
+export const ArrSub = (arr1, arr2) => {
+  return arr1.map((cur, ind) => cur - arr2[ind]);
+};
+
+// Given two arrays, tells if they have the exact same values
+export const Equals = (arr1, arr2) => {
+  let l = arr1.length;
+  if (arr2.length !== l) {
+    return false;
+  }
+  let keep = true;
+  for (let i = 0; i < l; i++) {
+    if (arr1[i] !== arr2[i]) {
+      keep = false;
+    }
+  }
+  return keep;
+};
+
+// AofA means Array of arrays
+export const RemoveDuplicatesOfSortedAofA = (arr) => {
+  return arr.filter((array, ind) => {
+    if (ind === 0) {
+      return true;
+    }
+    return !Equals(array, arr[ind - 1]);
+  });
+};
