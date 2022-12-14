3b8c52453663db4263000ae16ee90cdc0667d997
NuageTompis <88559558+NuageTompis@users.noreply.github.com>
2022-12-14 15:28:37 +0100

    Improved Day 13 Part 2

--- 2022/Day13Part2.js
+++ 2022/Day13Part2.js
@@ -1,53 +1,28 @@
 import { readFileSync } from "fs";
+import { Equals, Prod } from "./modules/UT.js";
 console.log("hello world");
 
 // This script does the same as Part 1 except we also need to add the elements [[2]] and [[6]] and then sort the puzzle input
 let input = readFileSync("Day13.txt", "utf-8")
   .trim()
   .replace(/\n\r/g, "")
-  .split("\r\n");
-input = input.map((pair) => pair.split("\r\n")[0]);
-
-const MakeList = (int) => "[" + int + "]";
-
-const OB = "[";
-const CB = "]";
-const SmartSplit = (list) => {
-  let list2 = list.slice(1, list.length - 1).split("");
-  let OBc = 0;
-  let commaInds = Array(0);
-  for (let i = 0; i < list2.length; i++) {
-    if (list2[i] == OB) {
-      OBc++;
-    }
-    if (list2[i] == CB) {
-      OBc--;
-    }
-    if (OBc === 0 && list2[i] == ",") {
-      commaInds.push(i);
-    }
-  }
-  for (let j = 0; j < commaInds.length; j++) {
-    list2[commaInds[j]] = "Z";
-  }
-  list2 = list2.join("").split("Z");
-  return list2;
-};
+  .split("\r\n")
+  .map(JSON.parse)
+  .concat([[[2]], [[6]]]);
 
 const CompareLists = (left, right) => {
-  if (left[1] === "]" && right[1] !== "]") {
+  if (left.length === 0 && right.length !== 0) {
     return true;
   }
-  if (left[1] !== "]" && right[1] === "]") {
+  if (left.length !== 0 && right.length === 0) {
     return false;
   }
-  let left2 = SmartSplit(left);
-  let right2 = SmartSplit(right);
   let keep = true;
-  let l = left2.length;
-  l = right2.length < l ? right2.length : l;
-  for (let i = 0; i < l && keep; i++) {
-    let comp = Compare(left2[i], right2[i]);
+  let ll = left.length;
+  let rl = right.length;
+  let min_l = rl < ll ? rl : ll;
+  for (let i = 0; i < min_l && keep; i++) {
+    let comp = Compare(left[i], right[i]);
     if (comp === true) {
       return true;
     }
@@ -55,32 +30,39 @@ const CompareLists = (left, right) => {
       return false;
     }
   }
-  if (left2.length < right2.length) return true;
-  if (left2.length > right2.length) return false;
+  if (left.length < rl) return true;
+  if (left.length > rl) return false;
   return 0;
 };
 
 const Compare = (left, right) => {
-  if (left[0] === "[" && right[0] === "[") {
+  if (typeof left !== "number" && typeof right !== "number") {
     return CompareLists(left, right);
-  } else if (left[0] === "[") {
-    return CompareLists(left, MakeList(right));
-  } else if (right[0] === "[") {
-    return CompareLists(MakeList(left), right);
+  } else if (typeof left !== "number") {
+    return CompareLists(left, [right]);
+  } else if (typeof right !== "number") {
+    return CompareLists([left], right);
   } else {
-    if (Number(left) < Number(right)) {
+    if (left < right) {
       return true;
     }
-    return Number(left) > Number(right) ? false : 0;
+    return left > right ? false : 0;
   }
 };
-
-input.push("[[2]]");
-input.push("[[6]]");
 
 input = input.sort((line1, line2) => {
   if (Compare(line1, line2)) return -1;
   return 1;
 });
 
-console.log((input.indexOf("[[2]]") + 1) * (input.indexOf("[[6]]") + 1));
+let indexes = Array(0);
+for (let i = 0; i < input.length; i++) {
+  if (Equals(input[i], [[2]])) {
+    indexes.push(i + 1);
+  } else if (Equals(input[i], [[6]])) {
+    indexes.push(i + 1);
+    break;
+  }
+}
+
+console.log(indexes.reduce(Prod, 1));
